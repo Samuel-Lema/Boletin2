556178adf6fab1cf0be3495ce7e8615f5c779e92
Samuel Lema <samuelhunter007@gmail.com>
2017-10-09 09:48:00 +0200

    Add files via upload

---
+++ Ticket_Bar/Ticket_Bar.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticket_bar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author slemagonzalez
+ */
+public class Ticket_Bar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String barName = "Bar Pepe";
+        double cocaCola = 2.5, auga = 1, pagado;
+        int nCocaCola, nAuga;
+        Scanner reader = new Scanner(System.in);
+        double total;
+        
+        System.out.print("Nº de coca colas compradas: ");
+        nCocaCola = reader.nextInt();
+        System.out.print("Nº de augas compradas: ");
+        nAuga = reader.nextInt();
+        System.out.print("Dinero pagado: ");
+        pagado = reader.nextDouble();
+        
+        total = (nCocaCola * cocaCola) + (nAuga * auga);
+        
+        System.out.println("\r\n|     " + barName);
+        if (nCocaCola > 0) {
+            System.out.println("| " + nCocaCola + " Colas --> " + (nCocaCola * cocaCola));
+        }
+        if (nAuga > 0) {
+            System.out.println("| " + nAuga + " augas --> " + (nAuga * auga));
+        }
+        System.out.println("|             ____");
+        System.out.println("| Total       " + total + "\r\n|");
+        System.out.println("| Entregado   " + pagado);
+        System.out.println("| Devolto     " + (pagado - total));
+        System.out.println("|   Grazas por vir   \r\n|");
+        System.out.println("|___________________|");
+    }
+    
+}
